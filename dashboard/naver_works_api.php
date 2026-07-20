687b680f9c73328f80934c682d0d7d62047d2447
USER <user@example.com>
2026-07-20 16:02:12 +0900

    Fix board post author field parsing (userName)

--- dashboard/naver_works_api.php
+++ dashboard/naver_works_api.php
@@ -578,7 +578,7 @@ if ($action === 'getBoardNotices') {
         $notices[] = [
             "postId" => $p['postId'],
             "title" => isset($p['title']) ? $p['title'] : '(제목 없음)',
-            "author" => isset($p['writer']['name']) ? $p['writer']['name'] : '알 수 없음',
+            "author" => isset($p['userName']) ? $p['userName'] : '알 수 없음',
             "createdTime" => isset($p['createdTime']) ? formatWorksMailTime($p['createdTime']) : '',
             "link" => "https://board.worksmobile.com/main/article/{$p['postId']}?boardNo={$targetBoardId}"
         ];
